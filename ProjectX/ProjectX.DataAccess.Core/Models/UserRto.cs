89cd8592af0f3ebb6650f6d8a9b87f204cddf88d
Tatyana-Essentya <dlyarubicona@gmail.com>
2021-11-14 17:55:10 +0300

    MicroFix

---
+++ ProjectX/ProjectX.DataAccess.Core/Models/UserRto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectX.DataAccess.Core.Models
+{
+    [Table("User")]
+    class UserRto
+    {
+      [Key] public int id { get; set; } 
+      public string Email { get; set; }
+      public string Login { get; set; }
+      [Required]public string PhoneNumberPrefix { get; set; }
+      [Required]public string PhoneNumber { get; set; }
+      public string FirstName { get; set; }
+      public string LastName { get; set; }
+      public string Patronymic { get; set; }
+      public DateTimeOffset Birthday { get; set; }
+      public string AvatarUrl { get; set; }
+      [Required]public UserRoleRto Role { get; set; }
+    }
+}
